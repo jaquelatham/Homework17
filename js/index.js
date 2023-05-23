b6a651a8fcac47e8d75ca31eb03255d6c01c4144
jaquelatham <120324804+jaquelatham@users.noreply.github.com>
2023-05-22 20:58:19 -0400

    js
    js test

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.view-case-study').on('click', function() {
+      window.location.href = '../Homework17/work.html'; // Replace with the URL of the desired page
+    });
+  });
+  
